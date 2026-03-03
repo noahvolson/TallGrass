c7b035d7a7be25fe6c116df08ba52acff8234fcc
Noah Olson <noah.v.olson@gmail.com>
2026-03-02 23:27:39 -0500

    Added catch button with cooldown

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import os
 import random
 import subprocess
+import time
 
 import discord
 import requests
@@ -16,6 +17,8 @@ token = os.getenv('DISCORD_TOKEN')
 pokemon_count = int(os.getenv('POKEMON_COUNT'))
 poke_api_url = os.getenv('POKE_API_URL')
 log_level = int(os.getenv('LOG_LEVEL'))
+catch_cooldown_sec = int(os.getenv('CATCH_COOLDOWN_SEC'))
+
 # TODO warn if any are not set
 
 # Init logging to discord.log
@@ -27,6 +30,41 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+# The ID of a Pokémon available for catching
+spawned_pokemon_id = -1
+
+class CatchView(discord.ui.View):
+
+    # user_id -> (last_click_time, last_message)
+    cooldowns = {}
+
+    @discord.ui.button(label="Throw a Poké Ball!", style=discord.ButtonStyle.primary)
+    async def button_callback(
+        self,
+        interaction: discord.Interaction,
+        button: discord.ui.Button
+    ):
+        user_id = interaction.user.id
+        now = time.monotonic()
+
+        last_click, last_msg = self.cooldowns.get(user_id, (0, None))
+        remaining = catch_cooldown_sec - (now - last_click)
+
+        if remaining > 0 and last_msg:
+            # Edit the previous message instead of sending a new one
+            await last_msg.edit(content=f"⏳ Slow down! Try again in {remaining:.1f}s.")
+            await interaction.response.defer()  # Acknowledge the interaction without sending a new message
+            return
+        elif last_msg:
+            await last_msg.delete()
+
+        # Send a new message
+        msg = await interaction.response.send_message("Button clicked!", ephemeral=True)
+        sent_msg = await interaction.original_response()  # Get the actual message object
+
+        # Store the time and message object
+        self.cooldowns[user_id] = (now, sent_msg)
 
 # Extend commands.Bot to schedule Pokémon spawning
 class TallGrass(commands.Bot):
@@ -61,8 +99,11 @@ class TallGrass(commands.Bot):
         embed = discord.Embed(title=f"Wild {name} appears!", color=discord.Color.dark_green())
         embed.set_image(url="attachment://pokemon.gif")
 
+        # Add catch button
+        view = CatchView()
+
         logger.info(f'Spawning {name} in channel: {self.channel.name}')
-        await self.channel.send(embed=embed, file=file)
+        await self.channel.send(embed=embed, file=file, view=view)
 
     async def setup_hook(self):
         self.spawner_task.start()
