eabc3bfca68ab5abc1820143fd85f0ec89d6525e
Noah Olson <noah.v.olson@gmail.com>
2026-03-06 23:37:41 -0500

    Move CatchView out of main bot script

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import asyncio
 import io
 import logging
 import os
 import random
 import subprocess
-import time
+
 import database
+import catch_view
 
 import discord
 import requests
-from discord import NotFound
 
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
@@ -20,7 +19,6 @@ token                       = os.getenv('DISCORD_TOKEN')
 poke_api_url                = os.getenv('POKE_API_URL')
 pokemon_count               = int(os.getenv('POKEMON_COUNT'))
 log_level                   = int(os.getenv('LOG_LEVEL'))
-catch_cooldown_sec          = int(os.getenv('CATCH_COOLDOWN_SEC'))
 rare_chance_percent         = int(os.getenv('RARE_CHANCE_PERCENT'))
 shiny_chance_percent        = int(os.getenv('SHINY_CHANCE_PERCENT'))
 regular_chance_percent      = int(os.getenv('REGULAR_CHANCE_PERCENT'))
@@ -37,95 +35,6 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-
-class CatchView(discord.ui.View):
-
-    def __init__(self, spawned_pokemon_id, spawned_pokemon_name, spawned_pokemon_catch_percent, sprite_url, is_shiny, *args, **kwargs):
-
-        self.cooldowns = {} # user_id -> (last_click_time, last_message)
-        self.claimed = False
-        self.claim_lock = asyncio.Lock()
-
-        # Details of the Pokémon available for catching
-        self.spawned_pokemon_id = spawned_pokemon_id
-        self.spawned_pokemon_name = spawned_pokemon_name
-        self.spawned_pokemon_catch_percent = spawned_pokemon_catch_percent
-        self.sprite_url = sprite_url
-        self.is_shiny = is_shiny
-
-        super().__init__(*args, **kwargs)
-
-    @discord.ui.button(label='Throw a Poké Ball!', style=discord.ButtonStyle.primary)
-    async def button_callback(
-        self,
-        interaction: discord.Interaction,
-        button: discord.ui.Button
-    ):
-        async with self.claim_lock:
-            if self.claimed:
-                await interaction.response.send_message(f'Too slow! {self.spawned_pokemon_name} already caught.', ephemeral=True)
-                return
-
-            user_id = interaction.user.id
-            now = time.monotonic()
-
-            last_click, last_msg = self.cooldowns.get(user_id, (0, None))
-            remaining = catch_cooldown_sec - (now - last_click)
-
-            if last_msg:
-                # Need to acknowledge the interaction within 3 seconds or discord invalidates
-                await interaction.response.defer()
-                if remaining > 0:
-                    try:
-                        await last_msg.edit(content=f':hourglass: Slow down! Try again in {remaining:.1f}s.')
-                    except discord.NotFound:
-                        pass
-                    return
-                else:
-                    try:
-                        await last_msg.delete()
-                    except discord.NotFound:
-                        pass
-
-            # Attempt to catch
-            roll = random.randint(1, 100)
-            success = roll <= self.spawned_pokemon_catch_percent
-            logger.debug(f'{interaction.user.display_name} Rolled: {roll}, Required: {self.spawned_pokemon_catch_percent} or lower')
-
-            if success:
-                try:
-                    await database.add_user_pokemon(
-                        interaction.user.id,
-                        self.spawned_pokemon_id,
-                        self.spawned_pokemon_name,
-                        self.spawned_pokemon_name,
-                        self.sprite_url,
-                        self.is_shiny
-                    )
-                except Exception as e:
-                    logger.error(f'Failed to add {self.spawned_pokemon_name} to user {interaction.user.id}: {e}')
-                    return
-
-                self.claimed = True
-                button.disabled = True
-                if last_msg:
-                    await interaction.edit_original_response(view=self)
-                else:
-                    await interaction.response.edit_message(view=self)
-                self.cooldowns = {}
-                message_string = f'Gotcha! {self.spawned_pokemon_name} was caught by {interaction.user.display_name}!'
-                await interaction.followup.send(message_string)
-                logger.info(f'{self.spawned_pokemon_name} was caught by {interaction.user.display_name}')
-            else:
-                message_string = 'Aww! It appeared to be caught!'
-                if last_msg:
-                    sent_msg = await interaction.followup.send(message_string, ephemeral=True)
-                else:
-                    await interaction.response.send_message(message_string, ephemeral=True)
-                    sent_msg = await interaction.original_response()
-
-                # Store the time and message object
-                self.cooldowns[user_id] = (now, sent_msg)
 
 # Extend commands.Bot to schedule Pokémon spawning
 class TallGrass(commands.Bot):
@@ -174,7 +83,7 @@ class TallGrass(commands.Bot):
         embed = discord.Embed(title=f'Wild {spawned_pokemon_name} appears!', color=discord.Color.dark_green())
         embed.set_image(url='attachment://pokemon.gif')
 
-        view = CatchView(
+        view = catch_view.CatchView(
             spawned_pokemon_id=spawned_pokemon_id,
             spawned_pokemon_name=spawned_pokemon_name,
             spawned_pokemon_catch_percent=spawned_pokemon_catch_percent,
